d1087c66150b077b3e8835b995058ca25858359b
ElonTusk404 <igmaflappy@gmail.com>
2023-06-01 01:26:11 +0400

    Create More_Variables_and_Printing.py

---
+++ More_Variables_and_Printing.py
@@ -0,0 +1,18 @@
+my_name = 'Зед Шоу'
+my_age = 35
+my_height = 188 
+my_weight = 80 
+my_eyes = 'Голубые'
+my_teeth = 'Белые'
+my_hair = 'Каштановые'
+print(f"Давайте поговорим о человеке по имени {my_name}")
+print(f"Его рост составляет {my_height} см")
+print(f"Он весит {my_weight} кг.")
+print(f"На самом деле это не так много")
+print(f"У него {my_eyes} глаза и {my_hair} волосы")
+print(f"Его зубы обычно {my_teeth}, хотя он и любит пить кофе.")
+print(f"Если я сложу {my_age} {my_height} {my_weight}, то получу {my_age+my_height+my_weight}")
+sm = 40
+kg = 5
+print(f"{sm}см. в дюймах это {sm*0.39}")
+print(f"{kg}кг. в фунтах это {kg*2.2}")
